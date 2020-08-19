2eff9d3fffc55cb5e32acf91e0db4852a6e6e07b
Mohamedvall <vaoooollm@gmail.com>
2020-08-19 03:12:55 -1200

    added User Profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
+  
   devise_for :users, controllers: { registrations: 'registrations' }
   root 'pages#timeline'
+
+  resources :users, only: [:show]
 
   get 'pages/timeline'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
