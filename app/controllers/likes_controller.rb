a0300593cf27ac5b895d0c6a8efa67affe137a2c
mohamedvall ibrahim <forvall.me@gmail.com>
2021-09-24 08:12:49 -1200

    show likes and dislikes in timeline

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,17 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
   before_action :find_likeable
+  before_action :destroy_likeable_activity
+
   def create
     @likeable.liked_by current_user
+    @likeable.create_activity key: 'tweet.like', owner: current_user, recipient: @likeable.user
     render 'update_likes_and_dislikes'
   end
-
+  
   def destroy
     @likeable.disliked_by current_user
+    @likeable.create_activity key: 'tweet.dislike', owner: current_user, recipient: @likeable.user
     render 'update_likes_and_dislikes'
   end
 
@@ -16,5 +20,10 @@ class LikesController < ApplicationController
   def find_likeable
     @likeable_type = params[:likeable_type].classify
     @likeable = @likeable_type.constantize.find(params[:likeable_id])
+  end
+
+  def destroy_likeable_activity
+    @likeable.activities.where(owner: current_user, key: 'tweet.like').first&.destroy
+    @likeable.activities.where(owner: current_user, key: 'tweet.dilike').first&.destroy
   end
 end
