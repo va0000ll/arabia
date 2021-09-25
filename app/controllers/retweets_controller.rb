907e3e890d41fa61751f298963cd34dd7c85faac
mohamedvall ibrahim <forvall.me@gmail.com>
2021-09-25 07:12:54 -1200

    Add retweets to activities

--- app/controllers/retweets_controller.rb
+++ app/controllers/retweets_controller.rb
@@ -4,6 +4,7 @@ class RetweetsController < ApplicationController
   def create
     retweet = current_user.retweets.new(source_tweet_id: params[:tweet_id])
     if retweet.save
+      retweet.create_activity key: 'retweet.created', owner: current_user, recipient: retweet.source_tweet.user
       redirect_to user_path(current_user.username), notice: 'تم اعادة التغريدة'
     else
       redirect_to fullback_location: root_path, alert: 'حدث خطأ ما!'
