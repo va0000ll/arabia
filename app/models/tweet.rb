274b0b84319c1b3fc028b25435e6bb614ca55010
mohamedvall ibrahim <forvall.me@gmail.com>
2021-09-25 07:07:16 -1200

    retwet views

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,6 +5,7 @@ class Tweet < ApplicationRecord
   acts_as_commentable
 
   belongs_to :user
+  has_many :retweets, dependent: :destroy, foreign_key: 'source_tweet_id'
 
   validates_presence_of :content
   validates_length_of :content, maximum: 140
