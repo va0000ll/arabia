26db61aea03cb1a72bd6c35305dab9b75b700eed
mohamedvall ibrahim <forvall.me@gmail.com>
2021-09-26 09:33:16 -1200

    add notifications controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :get_notifications
+
+  def get_notifications
+    @notifications = PublicActivity::Activity.where(recipient: current_user, readed: false).where.not(
+      owner: current_user, trackable: nil
+    ).order(created_at: desc)
+  end
 end
